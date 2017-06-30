c2a6bbd8e85d19b6b182acbc905fa28a8e643cca
EvliveTenshing <raya.rahadian322@gmail.com>
2017-06-30 19:47:38 +0700

    Version 0
    Added Database (Still in development)

---
+++ app/src/main/java/pemula/simpus/MainActivity.java
@@ -0,0 +1,15 @@
+package pemula.simpus;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import
+
+    public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
---
+++ app/src/main/java/pemula/simpus/SignUpActivity.java
@@ -0,0 +1,9 @@
+package pemula.simpus;
+
+/**
+ * Created by Evlive-Tenshi-PC on 6/30/2017.
+ */
+
+public class SignUpActivity {
+
+}
